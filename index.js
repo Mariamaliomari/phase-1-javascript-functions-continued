da3b9343a69d1187ec5cac32b3c2e86f89867205
Omari99 <mariam.ali01@student.moringaschool.com>
2022-11-24 23:50:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(game="roller-skate"){
+    return (`This Saturday, I want to ${game}!`)
+}
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+function wrapAdjective(wrapper){
+    switch (wrapper){
+        case "*":
+            return (function (){
+                return `You are ${wrapper}a hard worker${wrapper}!`
+            })
+       case "||":
+       return  (function(){
+        return `You are ${wrapper}a dedicated programmer${wrapper}!`
+       })    
+    }
+
+
+}
